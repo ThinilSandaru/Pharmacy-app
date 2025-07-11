5b943a321b74927fcf3e066cd3d85ca504da446c
Thinil <thinilsandaru@gmai.com>
2025-07-11 20:53:59 +0530

    Backend added

---
+++ Pharmacy/src/main/java/com/example/Farmacy/FarmacyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Farmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmacyApplication.class, args);
+	}
+
+}
---
+++ Pharmacy/src/test/java/com/example/Farmacy/FarmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Farmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
