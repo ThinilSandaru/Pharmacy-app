95613b2104942226aeffadfbec0b657e80ae1f92
Thinil <thinilsandaru@gmai.com>
2025-07-12 10:23:30 +0530

    Backend files added

--- Farmacy/src/main/java/com/example/Farmacy/FarmacyApplication.java
+++ Farmacy/src/main/java/com/example/Farmacy/FarmacyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FarmacyApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(FarmacyApplication.class, args);
 	}
 
